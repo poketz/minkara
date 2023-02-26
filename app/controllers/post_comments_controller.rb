e942f93df0e8faf52f618fa9a210ba5bdeaa18b5
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2023-02-26 09:41:06 +0000

    [Add] エラーメッセージの実装

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,18 +1,24 @@
 class PostCommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
+    @post_comments = @post.post_comments.all
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.user_id = current_user.id
     @post_comment.post_id = params[:post_id]
-    @post_comment.save
-    flash.now[:primary] = 'コメントを投稿しました'
-    @post_comments = @post.post_comments.all
+    if @post_comment.save
+      flash.now[:primary] = 'コメントを投稿しました'
+    # else
+    #   render 'error'
+    end
+
   end
 
   def destroy
     post_comment = PostComment.find(params[:id])
     post_comment.destroy
     flash.now[:primary] = "コメントを削除しました"
+    @post = Post.find(params[:post_id])
+    @post_comments = @post.post_comments.all
   end
 
   private
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,4 +2,6 @@ class PostComment < ApplicationRecord
   has_one :notification, as: :subject, dependent: :destroy
   belongs_to :post
   belongs_to :user
+
+  validates :body, presence: true, length: {maximum: 150}
 end
