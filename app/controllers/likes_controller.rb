00dcd24cf0061d48ce3e8a477637c033cd7f0ec4
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2023-02-26 05:08:27 +0000

    [Fix] 非同期通信機能の修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,9 @@
 class LikesController < ApplicationController
   def create
+    pos = Post.find(params[:post_id])
+    like = current_user.likes.new(post_id: pos.id)
+    like.save!
+    redirect_to post_path(pos)
   end
 
   def destroy
--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,19 +4,15 @@ class PostCommentsController < ApplicationController
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.user_id = current_user.id
     @post_comment.post_id = params[:post_id]
-    if @post_comment.save
-      flash[:primary] = "コメントを投稿しました"
-      render :post_comments
-    else
-      render 'posts/show'
-    end
+    @post_comment.save
+    flash.now[:primary] = 'コメントを投稿しました'
+    @post_comments = @post.post_comments.all
   end
 
   def destroy
     post_comment = PostComment.find(params[:id])
     post_comment.destroy
-    flash[:primary] = "コメントを削除しました"
-    redirect_to post_path(params[:post_id])
+    flash.now[:primary] = "コメントを削除しました"
   end
 
   private
