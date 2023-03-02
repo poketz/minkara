0521ecf3816c5d06a671558fe8659b2472bc0efb
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2023-03-02 04:21:15 +0000

    [Update] 細かいレイアウト調整

--- app/controllers/searches_controller.rb
+++
@@ -1,8 +0,0 @@
-class SearchesController < ApplicationController
-  def post_search
-    @posts = Post.search(params[:word])
-  end
-
-  def user_search
-  end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = Post.all
+    @posts = @user.posts.all
   end
 
   def show_info
