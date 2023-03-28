2f461c7e25464e4e78192c121e5271d391413c21
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2023-03-28 09:12:37 +0000

    [Test] テストデータの挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create!(
+  [
+    {name: 'ひろし', password: 'password', email: 'e@mail', gender: 0, prefecture: 15, birthday: '1995-10-10'},
+    {name: 'みほ', password: 'password', email: 'e@mail2', gender: 1, prefecture: 11, birthday: '1988-06-23'},
+    {name: 'りさ', password: 'password', email: 'e@mail3', gender: 1, prefecture: 13, birthday: '1991-12-02'},
+    {name: 'たけひと', password: 'password', email: 'e@mail4', gender: 0, prefecture: 15, birthday: '1956-05-19'},
+    {name: 'よしひろ', password: 'password', email: 'e@mail5', gender: 0, prefecture: 13, birthday: '1985-08-05'},
+  ]
+)
