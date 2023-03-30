9aa516045d6833b7ad3949c7e9500f6f4ba83d76
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2023-03-30 15:08:00 +0000

    [Fix] ランキング機能の不具合の修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,7 @@ class HomesController < ApplicationController
   def top
     @posts = Post.order("created_at DESC").limit(5)
     # 週間ランキング
-    @week_post_like_ranks = Post.where(created_at: Time.current.all_week).limit(5).sort { |a, b| b.likes.count <=> a.likes.count }
+    @week_post_like_ranks = Post.find(Like.group(:post_id).where(created_at: Time.current.all_week).order('count(post_id) desc').limit(5).pluck(:post_id))
   end
 
   def about
