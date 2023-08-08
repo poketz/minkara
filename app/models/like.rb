9caeb6ed6b653f0309ba3a681736792fa701de12
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2023-08-08 15:27:40 +0000

    [Refactor] トップページのリファクタリング

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  
+  scope :liked_this_week, -> { where(created_at: Time.current.all_week) } 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,6 @@ class Post < ApplicationRecord
   mount_uploader :audio, AudioUploader
   validates :audio, presence: { message: "を選択してください" }
   enum open_range: { public: 0, follow: 1, private: 2 }, _prefix: true  # 文末はエラー回避のため
-  
-  scope :created_this_week, -> { where(created_at: Time.current.all_week) } 
 
   def self.search(search, word)
     # キーワード分割
